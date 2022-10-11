f179221ac05b07c1e4ab4d352ca8f5c0e9f4faf0
Orkhansha <orkhansha@code.edu.az>
2022-10-11 15:03:59 -0700

    Added loadmore files

---
+++ Front-to-back/Front-to-back/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Front_to_back.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Front_to_back.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Accessory> Accessories { get; set; }
+          
+    }
+}
